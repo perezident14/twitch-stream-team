246c5cc80ae6914bba2d51af564a9f2053b27077
Joseph Perez <joseph@p14.dev>
2021-07-01 11:21:12 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 
+import Header from './components/Header'
 import Games from './components/Games'
 
 function App() {
   return (
     <Router>
+      <Header />
       <Route exact path='/' component={Games} />
     </Router>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1>
+      Twitch Approach
+    </h1>
+  )
+}
+
+export default Header
