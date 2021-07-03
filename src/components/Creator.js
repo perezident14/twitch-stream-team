bfaa3dd389288612f7fc03b598ca82b82972a951
Joseph Perez <joseph@p14.dev>
2021-07-03 13:50:55 -0700

    concept created

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../api'
 
-function Creator() {
+function Creator(props) {
+
+  const [creator, setCreator] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const info = await api.get(`https://api.twitch.tv/helix/users?login=${props.name}`)
+      // console.log(info.data.data[0])
+      setCreator(info.data.data[0])
+    }
+    fetchData()
+  })
+
   return (
     <div>
-      
+      {creator.login}
     </div>
   )
 }
--- src/containers/CreatorsContainer.js
+++ src/containers/CreatorsContainer.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react'
-import api from '../api'
+import Creator from '../components/Creator'
 
 class CreatorsContainer extends Component {
   constructor() {
     super()
     this.state = {
-      creators: []
     }
   }
-
-  // componentDidMount() {
-  //   const fetchData = async () => {
-  //     const result = await api.get('https://api.twitch.tv/helix/teams?name=astro')
-  //     console.log(result.data.data[0])
-  //   }
-  //   fetchData()
-  // }
 
   render() {
     return (
       <div>
-        Creators
+        <h1>{this.props.astro.displayName}</h1>
+        {this.props.astro.creators.map(creator => {
+          return (
+            <Creator name={creator.user_login} />
+          )
+        })}
       </div>
     )
   }
