68e0c021863855340a031146f05c0be4fa6ec0d9
Joseph Perez <joseph@p14.dev>
2021-07-06 17:39:32 -0700

    cleanup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Contact.css'
 
 function Contact() {
   return (
-    <div>
-      
+    <div className='contact-container'>
+      <div className='contact-info'>
+        <h3>Contact Email:</h3>
+        <a href='mailto:thecollective@astrogaming.com'>thecollective@astrogaming.com</a>
+      </div>
     </div>
   )
 }
